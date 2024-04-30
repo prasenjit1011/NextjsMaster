5b3164d78c38099730d3e0e5c48c19e79209bcd0
Riyan <riyan04314@gmail.com>
2024-04-30 06:53:28 +0530

    Blog-Slug

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import styles from "../../page.module.css";
 
-export default function UserBlogPage(){
+export default function UserBlogPage({params}){
+    console.log(789)
     return (
         <main className={styles.main}>
-            <h3>Blog [XXX]</h3>
+            <h3>Blog {params.slug}</h3>
             <Link href="/">Home</Link>
         </main>
     )
