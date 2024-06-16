267e03aa052ce0fd563dbbc5f295edc588dc5c1d
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-16 16:24:44 +0530

    Setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,30 +16,18 @@ export default function RootLayout({ children }) {
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <meta content="" name="keywords" />
         <meta content="" name="description" />
-
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com"  crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Raleway:wght@600;800&display=swap" rel="stylesheet" precedence="default" /> 
-
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" precedence="default"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" precedence="default" />
-
-
         <link href="html/lib/lightbox/css/lightbox.min.css" rel="stylesheet" precedence="default" />
         <link href="html/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" precedence="default" />
-
         <link href="html/css/bootstrap.min.css" rel="stylesheet" precedence="default" />
-
         <link href="html/css/style.css" rel="stylesheet" precedence="default" />
         <link rel="icon" type="image/x-icon" href="/html/img/bag.png" />
         <body >
-
-
-
             <AllComponent />
-
-            
-
             <a href="#" className="btn btn-primary border-3 border-primary rounded-circle back-to-top"><i className="fa fa-arrow-up"></i></a>   
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
@@ -49,30 +38,53 @@ export default function RootLayout({ children }) {
             <script src="html/js/main.js"></script>
         </body>
     </html>
-  
   );
 }
-
 
 
 function AllComponent(){
     return (
         <>
             <ComponentA />
+            <Header />
+            <ComponentM />
+            <Footer />
+        </>
+    );
+}
+
+
+function Header(){
+    return (
+        <></>
+    );
+    return (
+        <>
             <ComponentB />
             <ComponentC />
             <ComponentD />
             <ComponentE />
+        </>
+    );
+}
+
+function Footer(){
+    return (
+        <></>
+    );
+    return (
+        <>
             <ComponentF />
             <ComponentG />
             <ComponentH />
             <ComponentI />
             <ComponentJ />
             <ComponentK />
-            <ComponentL />
+            <ComponentL />            
         </>
     );
 }
+
 
 function ComponentA(){
     return (
@@ -890,6 +902,7 @@ function ComponentI(){
                     <h4 className="text-primary">Our Testimonial</h4>
                     <h1 className="display-5 mb-5 text-dark">Our Client Saying!</h1>
                 </div>
+                <ComponentTestimonial />
             </div>
         </div>
     );
@@ -1043,3 +1056,233 @@ function ComponentL(){
         </div>
     );
 }
+
+function ComponentM(){
+    return (
+        <div className="container-fluid vesitable py-5">
+            <div className="container py-5">
+                <h1 className="mb-0">Fresh Organic Vegetables</h1>
+                <ComponentOrganic />                
+            </div>
+        </div>
+    );
+}
+
+function ComponentOrganic(){
+    // return (
+    //     <h1>ComponentOrganic</h1>
+    // );
+
+    // return (
+    //     <>
+    //         <div className="owl-carousel vegetable-carousel justify-content-center">
+
+    //         </div>
+    //     </>
+    // );
+
+    return (
+        <div className="owl-carousel vegetable-carousel justify-content-center">
+            <div className="border border-primary rounded position-relative vesitable-item">
+                <div className="vesitable-img">
+                    <img src="html/img/vegetable-item-6.jpg" className="img-fluid w-100 rounded-top" alt="" />
+                </div>
+                <div className="text-white bg-primary px-3 py-1 rounded position-absolute" style={{top: "10px", right: "10px"}}>Vegetable</div>
+                <div className="p-4 rounded-bottom">
+                    <h4>Parsely</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+                    <div className="d-flex justify-content-between flex-lg-wrap">
+                        <p className="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
+                        <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div className="border border-primary rounded position-relative vesitable-item">
+                <div className="vesitable-img">
+                    <img src="html/img/vegetable-item-1.jpg" className="img-fluid w-100 rounded-top" alt="" />
+                </div>
+                <div className="text-white bg-primary px-3 py-1 rounded position-absolute" style={{top: "10px", right: "10px"}}>Vegetable</div>
+                <div className="p-4 rounded-bottom">
+                    <h4>Parsely</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+                    <div className="d-flex justify-content-between flex-lg-wrap">
+                        <p className="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
+                        <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div className="border border-primary rounded position-relative vesitable-item">
+                <div className="vesitable-img">
+                    <img src="html/img/vegetable-item-3.png" className="img-fluid w-100 rounded-top bg-light" alt="" />
+                </div>
+                <div className="text-white bg-primary px-3 py-1 rounded position-absolute" style={{top: "10px", right: "10px"}}>Vegetable</div>
+                <div className="p-4 rounded-bottom">
+                    <h4>Banana</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+                    <div className="d-flex justify-content-between flex-lg-wrap">
+                        <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
+                        <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div className="border border-primary rounded position-relative vesitable-item">
+                <div className="vesitable-img">
+                    <img src="html/img/vegetable-item-4.jpg" className="img-fluid w-100 rounded-top" alt="" />
+                </div>
+                <div className="text-white bg-primary px-3 py-1 rounded position-absolute" style={{top: "10px", right: "10px"}}>Vegetable</div>
+                <div className="p-4 rounded-bottom">
+                    <h4>Bell Papper</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+                    <div className="d-flex justify-content-between flex-lg-wrap">
+                        <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
+                        <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div className="border border-primary rounded position-relative vesitable-item">
+                <div className="vesitable-img">
+                    <img src="html/img/vegetable-item-5.jpg" className="img-fluid w-100 rounded-top" alt="" />
+                </div>
+                <div className="text-white bg-primary px-3 py-1 rounded position-absolute" style={{top: "10px", right: "10px"}}>Vegetable</div>
+                <div className="p-4 rounded-bottom">
+                    <h4>Potatoes</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+                    <div className="d-flex justify-content-between flex-lg-wrap">
+                        <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
+                        <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div className="border border-primary rounded position-relative vesitable-item">
+                <div className="vesitable-img">
+                    <img src="html/img/vegetable-item-6.jpg" className="img-fluid w-100 rounded-top" alt="" />
+                </div>
+                <div className="text-white bg-primary px-3 py-1 rounded position-absolute" style={{top: "10px", right: "10px"}}>Vegetable</div>
+                <div className="p-4 rounded-bottom">
+                    <h4>Parsely</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+                    <div className="d-flex justify-content-between flex-lg-wrap">
+                        <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
+                        <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div className="border border-primary rounded position-relative vesitable-item">
+                <div className="vesitable-img">
+                    <img src="html/img/vegetable-item-5.jpg" className="img-fluid w-100 rounded-top" alt="" />
+                </div>
+                <div className="text-white bg-primary px-3 py-1 rounded position-absolute" style={{top: "10px", right: "10px"}}>Vegetable</div>
+                <div className="p-4 rounded-bottom">
+                    <h4>Potatoes</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+                    <div className="d-flex justify-content-between flex-lg-wrap">
+                        <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
+                        <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div className="border border-primary rounded position-relative vesitable-item">
+                <div className="vesitable-img">
+                    <img src="html/img/vegetable-item-6.jpg" className="img-fluid w-100 rounded-top" alt="" />
+                </div>
+                <div className="text-white bg-primary px-3 py-1 rounded position-absolute" style={{top: "10px", right: "10px"}}>Vegetable</div>
+                <div className="p-4 rounded-bottom">
+                    <h4>Parsely</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+                    <div className="d-flex justify-content-between flex-lg-wrap">
+                        <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
+                        <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+function ComponentTestimonial(){
+    return (
+        <h1>ComponentTestimonial</h1>
+    );
+
+    return (
+        <div className="owl-carousel testimonial-carousel">
+            <div className="testimonial-item img-border-radius bg-light rounded p-4">
+                <div className="position-relative">
+                    <i className="fa fa-quote-right fa-2x text-secondary position-absolute" style={{bottom: "30px", right: "0"}}></i>
+                    <div className="mb-4 pb-4 border-bottom border-secondary">
+                        <p className="mb-0">Lorem Ipsum is simply dummy text of the printing Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        </p>
+                    </div>
+                    <div className="d-flex align-items-center flex-nowrap">
+                        <div className="bg-secondary rounded">
+                            <img src="html/img/testimonial-2.jpg" className="img-fluid rounded" style={{width: "100px", height: "100px"}} alt="" />
+                        </div>
+                        <div className="ms-4 d-block">
+                            <h4 className="text-dark">Client Name</h4>
+                            <p className="m-0 pb-3">Profession</p>
+                            <div className="d-flex pe-5">
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="testimonial-item img-border-radius bg-light rounded p-4">
+                <div className="position-relative">
+                    <i className="fa fa-quote-right fa-2x text-secondary position-absolute" style={{bottom: "30px", right: "0"}}></i>
+                    <div className="mb-4 pb-4 border-bottom border-secondary">
+                        <p className="mb-0">Lorem Ipsum is simply dummy text of the printing Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        </p>
+                    </div>
+                    <div className="d-flex align-items-center flex-nowrap">
+                        <div className="bg-secondary rounded">
+                            <img src="html/img/testimonial-1.jpg" className="img-fluid rounded" style={{width: "100px", height: "100px"}} alt="" />
+                        </div>
+                        <div className="ms-4 d-block">
+                            <h4 className="text-dark">Client Name</h4>
+                            <p className="m-0 pb-3">Profession</p>
+                            <div className="d-flex pe-5">
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="testimonial-item img-border-radius bg-light rounded p-4">
+                <div className="position-relative">
+                    <i className="fa fa-quote-right fa-2x text-secondary position-absolute" style={{bottom: "30px", right: "0"}}></i>
+                    <div className="mb-4 pb-4 border-bottom border-secondary">
+                        <p className="mb-0">Lorem Ipsum is simply dummy text of the printing Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        </p>
+                    </div>
+                    <div className="d-flex align-items-center flex-nowrap">
+                        <div className="bg-secondary rounded">
+                            <img src="html/img/testimonial-1.jpg" className="img-fluid rounded" style={{width: "100px", height: "100px"}} alt="" />
+                        </div>
+                        <div className="ms-4 d-block">
+                            <h4 className="text-dark">Client Name</h4>
+                            <p className="m-0 pb-3">Profession</p>
+                            <div className="d-flex pe-5">
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                                <i className="fas fa-star text-primary"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
