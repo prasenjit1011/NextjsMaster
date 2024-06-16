485a04757e45bfaa05ab0fe012b4ff48712b0c17
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-16 07:54:23 +0530

    Setup

--- src/app/contactus/page.js
+++ src/app/contactus/page.js
@@ -183,7 +183,7 @@ function Contactus(){
                 I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.
             </p><br />
             <p>
-                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
             </p>
         </div>
     );
