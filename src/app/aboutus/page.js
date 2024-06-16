397a08f82cd1680d76ebbd0bdfec1662f6306e57
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-16 07:52:59 +0530

    About US

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -19,7 +19,7 @@ export default function AboutusPage(){
                         In the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment
                     </p><br />
                     <p>
-                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
                     </p>
                 </div>
             </div>
