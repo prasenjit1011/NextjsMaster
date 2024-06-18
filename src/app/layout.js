23126d835cee4724e782f155de277f0887f6e8f4
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-18 08:54:30 +0530

    setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -180,8 +180,19 @@ function ComponentA(){
             <div className="container topbar bg-primary d-none d-lg-block">
                 <div className="d-flex justify-content-between">
                     <div className="top-info ps-2">
-                        <small className="me-3"><i className="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#" className="text-white">123 Street, New York</a></small>
-                        <small className="me-3"><i className="fas fa-envelope me-2 text-secondary"></i><a href="#" className="text-white">Email@Example.com</a></small>
+                        <small className="me-3">
+                            <i className="fas fa-map-marker-alt me-2 text-secondary"></i> 
+                            <a href="#" className="text-white">
+                                230, Udyog Vihar Phase IV, Sector 18
+                                Gurugram - 122015
+                            </a>
+                        </small>
+                        <small className="me-3">
+                            <i className="fas fa-envelope me-2 text-secondary"></i>
+                            <a href="#" className="text-white">
+                                hello@tuliooutlet.shop
+                            </a>
+                        </small>
                     </div>
                     <div className="top-link pe-2">
                         <a href="#" className="text-white"><small className="text-white mx-2">Privacy Policy</small>/</a>
