1f7d759c041e2efb268f95046e14fef1bb03341e
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-18 17:37:19 +0530

    setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,22 +4,28 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <meta charSet="utf-8" />
-        <title>TuliOutlet - Vegetable Website Template</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-        <meta content="" name="keywords" />
-        <meta content="" name="description" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"  crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Raleway:wght@600;800&display=swap" rel="stylesheet" precedence="default" /> 
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" precedence="default"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" precedence="default" />
-        <link href="html/lib/lightbox/css/lightbox.min.css" rel="stylesheet" precedence="default" />
+        <head>
+            <meta charSet="utf-8" />
+            <title>Kider</title>
+            <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+            <meta content="" name="keywords" />
+            <meta content="" name="description" />
         
-        <link href="html/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" precedence="default" />
-        <link href="html/css/bootstrap.min.css" rel="stylesheet" precedence="default" />
-        <link href="html/css/style.css" rel="stylesheet" precedence="default" />
-        <link rel="icon" type="image/x-icon" href="/html/img/bag.png" />
+            <link href="kider/img/favicon.ico" rel="icon" />
+        
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+            <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@600&family=Lobster+Two:wght@700&display=swap" rel="stylesheet" />
+
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet" />
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+            
+            <link href="kider/lib/animate/animate.min.css" rel="stylesheet" />
+            <link href="kider/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+            
+            <link href="kider/css/bootstrap.min.css" rel="stylesheet" />    
+            <link href="kider/css/style.css" rel="stylesheet" />
+        </head>
         <body >
             <AllComponent />
 
@@ -42,6 +48,6 @@ export default function RootLayout({ children }) {
 
 function AllComponent(){
     return (
-        <h1>All Component 02</h1>
+        <h1>All Component 03</h1>
     );
 }
