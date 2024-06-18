e61fb529cc7f7a4153024c734ae7157ffbf4cbb8
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-18 18:30:56 +0530

    setup

---
+++ src/app/classes/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+import { Classroom } from "@/components/classroom";
+
+export default function AboutusPage(){
+    return (
+        <Classroom />
+    )
+}
