c24e9718c71fe9ab2ea5a369dc5884a49599826a
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-18 18:21:15 +0530

    setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import {SiteMenu, Loader, Banner, Facility, LearnMore, BecomeTeacher, Appointment, Teams, Testimonial, TestimonialList, Footer, ArrowBtn} from "@/components/common";
 
 export default function Home() {
   return (
-    <h1>Page 2</h1>
+    <>
+      <Banner />
+      <Facility />
+      <LearnMore />
+      <BecomeTeacher />
+      <Appointment />
+      <Teams />
+      <Testimonial />
+    </>
   );
 }
