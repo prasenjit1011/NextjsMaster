b7329014a0eb4767c40e1d0b1d7f2b3f9cafe210
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-18 18:31:47 +0530

    setup

---
+++ src/app/appointment/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+import { Appointment } from "@/components/common";
+
+export default function AboutusPage(){
+    return (
+        <Appointment />
+    )
+}
