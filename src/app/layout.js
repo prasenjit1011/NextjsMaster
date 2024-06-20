57101725665379c6b42a29cd2dacc3e69e515fef
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-20 08:19:24 +0530

    Setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1144,11 +1144,9 @@ function ComponentL(){
             <div className="container">
                 <div className="row">
                     <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        <span className="text-light"><a href="#"><i className="fas fa-copyright text-light me-2"></i>Your Site Name</a>, All right reserved.</span>
+                        <span className="text-light"><a href="#"><i className="fas fa-copyright text-light me-2"></i>TuliOutlet</a>, All right reserved.</span>
                     </div>
                     <div className="col-md-6 my-auto text-center text-md-end text-white">
-                        
-                        Designed By <a className="border-bottom" href="https://htmlcodex.com">HTML Codex</a> Distributed By <a className="border-bottom" href="https://themewagon.com">ThemeWagon</a>
                     </div>
                 </div>
             </div>
