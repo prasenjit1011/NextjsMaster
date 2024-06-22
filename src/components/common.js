1088ad616df3bf327614ce0e58ecde60e0e2b500
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-22 08:36:54 +0530

    commit

--- src/components/common.js
+++ src/components/common.js
@@ -31,39 +31,39 @@ export function Header(){
                         </small>
                     </div>
                     <div className="top-link pe-2">
-                        <a href="#" className="text-white"><small className="text-white mx-2">Privacy Policy</small>/</a>
-                        <a href="#" className="text-white"><small className="text-white mx-2">Terms of Use</small>/</a>
-                        <a href="#" className="text-white"><small className="text-white ms-2">Sales and Refunds</small></a>
+                        <Link href="#" className="text-white"><small className="text-white mx-2">Privacy Policy</small>/</Link>
+                        <Link href="#" className="text-white"><small className="text-white mx-2">Terms of Use</small>/</Link>
+                        <Link href="#" className="text-white"><small className="text-white ms-2">Sales and Refunds</small></Link>
                     </div>
                 </div>
             </div>
             <div className="container px-0">
                 <nav className="navbar navbar-light bg-white navbar-expand-xl">
-                    <a href="index.html" className="navbar-brand"><h1 className="text-primary display-6">TuliOutlet</h1></a>
+                    <Link href="/" className="navbar-brand"><h1 className="text-primary display-6">TuliOutlet</h1></Link>
                     <button className="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                         <span className="fa fa-bars text-primary"></span>
                     </button>
                     <div className="collapse navbar-collapse bg-white" id="navbarCollapse">
                         <div className="navbar-nav mx-auto">
-                            <a href="index.html" className="nav-item nav-link active">Home</a>
-                            <a href="shop.html" className="nav-item nav-link">Shop</a>
-                            <a href="shop-detail.html" className="nav-item nav-link">Shop Detail</a>
+                            <Link href="/" className="nav-item nav-link active">Home</Link>
+                            <Link href="shop" className="nav-item nav-link">Shop</Link>
+                            <Link href="product" className="nav-item nav-link">Product</Link>
                             <div className="nav-item dropdown" style={{display:"none"}}>
                                 <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                                 <div className="dropdown-menu m-0 bg-secondary rounded-0">
-                                    <a href="cart.html" className="dropdown-item">Cart</a>
-                                    <a href="chackout.html" className="dropdown-item">Chackout</a>
-                                    <a href="testimonial.html" className="dropdown-item">Testimonial</a>
-                                    <a href="404.html" className="dropdown-item">404 Page</a>
+                                    <Link href="cart.html" className="dropdown-item">Cart</Link>
+                                    <Link href="chackout.html" className="dropdown-item">Chackout</Link>
+                                    <Link href="testimonial.html" className="dropdown-item">Testimonial</Link>
+                                    <Link href="404.html" className="dropdown-item">404 Page</Link>
                                 </div>
                             </div>
-                            <a href="contact.html" className="nav-item nav-link">Contact</a>
+                            <Link href="contact.html" className="nav-item nav-link">Contact</Link>
                         </div>
                         <div className="d-flex m-3 me-0">
                             <button className="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" data-bs-toggle="modal" data-bs-target="#searchModal"><i className="fas fa-search text-primary"></i></button>
                             <a href="#" className="position-relative me-4 my-auto">
                                 <i className="fa fa-shopping-bag fa-2x"></i>
-                                <span className="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style={{top:"-5px", left:"15px", height:" 20px", minWidth:" 20px"}} >3</span>
+                                
                             </a>
                             <a href="#" className="my-auto">
                                 <i className="fas fa-user fa-2x"></i>
