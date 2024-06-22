4a3f9551c04eb4c4f715455bc79557579c5c2f93
Prasenjit <prasenjit.aluni@gmail.com>
2024-06-22 19:03:36 +0530

    setup

--- src/components/common.js
+++ src/components/common.js
@@ -995,66 +995,16 @@ function ProductFilterC(){
     
     
 function ProductFilterD(){
+    let products = [];
+    for (let i = 0; i < 4; i++) {
+        products.push(<ProductFeature key={i} imgName={"featur-"+i+".jpg"} />);
+    }
+
+
     return (
         <div className="col-lg-12">
             <h4 className="mb-3">Featured products</h4>
-            <div className="d-flex align-items-center justify-content-start">
-                <div className="rounded me-4" style={{width: "100px", height: "100px"}}>
-                    <img src="/eshop/img/featur-1.jpg" className="img-fluid rounded" alt="" />
-                </div>
-                <div>
-                    <h6 className="mb-2">Big Banana</h6>
-                    <div className="d-flex mb-2">
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star"></i>
-                    </div>
-                    <div className="d-flex mb-2">
-                        <h5 className="fw-bold me-2">2.99 $</h5>
-                        <h5 className="text-danger text-decoration-line-through">4.11 $</h5>
-                    </div>
-                </div>
-            </div>
-            <div className="d-flex align-items-center justify-content-start">
-                <div className="rounded me-4" style={{width: "100px", height: "100px"}}>
-                    <img src="/eshop/img/featur-2.jpg" className="img-fluid rounded" alt="" />
-                </div>
-                <div>
-                    <h6 className="mb-2">Big Banana</h6>
-                    <div className="d-flex mb-2">
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star"></i>
-                    </div>
-                    <div className="d-flex mb-2">
-                        <h5 className="fw-bold me-2">2.99 $</h5>
-                        <h5 className="text-danger text-decoration-line-through">4.11 $</h5>
-                    </div>
-                </div>
-            </div>
-            <div className="d-flex align-items-center justify-content-start">
-                <div className="rounded me-4" style={{width: "100px", height: "100px"}}>
-                    <img src="/eshop/img/featur-3.jpg" className="img-fluid rounded" alt="" />
-                </div>
-                <div>
-                    <h6 className="mb-2">Big Banana</h6>
-                    <div className="d-flex mb-2">
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star text-secondary"></i>
-                        <i className="fa fa-star"></i>
-                    </div>
-                    <div className="d-flex mb-2">
-                        <h5 className="fw-bold me-2">2.99 $</h5>
-                        <h5 className="text-danger text-decoration-line-through">4.11 $</h5>
-                    </div>
-                </div>
-            </div>
+            {products}
             <div className="d-flex justify-content-center my-4">
                 <a href="#" className="btn border border-secondary px-4 py-3 rounded-pill text-primary w-100">Vew More</a>
             </div>
@@ -1062,6 +1012,31 @@ function ProductFilterD(){
     );
 }
 
+function ProductFeature({imgName}){
+    return (
+        <div className="d-flex align-items-center justify-content-start">
+            <div className="rounded me-4" style={{width: "100px", height: "100px"}}>
+                <img src={"/eshop/img/"+imgName} className="img-fluid rounded" alt="" />
+            </div>
+            <div>
+                <Link href="/product/1">
+                    <h6 className="mb-2">Big Oranges</h6>
+                </Link>
+                <div className="d-flex mb-2">
+                    <i className="fa fa-star text-secondary"></i>
+                    <i className="fa fa-star text-secondary"></i>
+                    <i className="fa fa-star text-secondary"></i>
+                    <i className="fa fa-star text-secondary"></i>
+                    <i className="fa fa-star"></i>
+                </div>
+                <div className="d-flex mb-2">
+                    <h5 className="fw-bold me-2">2.99 $</h5>
+                    <h5 className="text-danger text-decoration-line-through">4.11 $</h5>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 function ProductFilterE(){
     return (
